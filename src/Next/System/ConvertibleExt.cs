1d917ddbae8b4dd14d30f5e51fbc05365fd20972
unknown <nikoloz@hotmail.com>
2014-11-24 16:58:00 +0400

    added IConvertible extension method

---
+++ src/Next/System/ConvertibleExt.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+// ReSharper disable once CheckNamespace
+
+
+namespace System
+{
+    /// <summary>
+    /// IConvertible extension methods
+    /// </summary>
+    public static class ConvertibleExt
+    {
+        /// <summary>
+        /// Converts value to string using invariant culture
+        /// </summary>
+        /// <param name="this">Source value</param>
+        /// <returns>Culture invariant representation of source value</returns>
+        public static string ToStringInvariant(this IConvertible @this)
+        {
+            return @this.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}

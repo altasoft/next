47ad01ec7478072028019f20df211473bb0e1fb1
unknown <nikoloz@hotmail.com>
2014-11-24 16:42:05 +0400

    added tests

--- Next/Properties/AssemblyInfo.cs
+++ Next/Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Next")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Extension methods for .Net")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ALTA Software")]
 [assembly: AssemblyProduct("Next")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]

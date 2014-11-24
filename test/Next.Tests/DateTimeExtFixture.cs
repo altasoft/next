0ef7bb06693fce08f07fe2df630ef0a8062278a2
unknown <nikoloz@hotmail.com>
2014-11-24 16:41:17 +0400

    Added tests

---
+++ test/Next.Tests/DateTimeExtFixture.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Next.Tests
+{
+    public sealed class DateTimeExtFixture
+    {
+        [Fact]
+        public void EndOfMonth()
+        {
+            Assert.Equal(new DateTime(2014, 10, 31), new DateTime(2014, 10, 23).EndOfMonth());
+            Assert.Equal(new DateTime(2016, 2, 29), new DateTime(2016, 2, 17).EndOfMonth());
+            Assert.Equal(new DateTime(2017, 2, 28), new DateTime(2017, 2, 17).EndOfMonth());
+            Assert.Equal(new DateTime(2014, 11, 30), new DateTime(2014, 11, 11).EndOfMonth());
+        }
+
+        [Fact]
+        public void DateTimeIsEndOfMonth()
+        {
+            Assert.True(new DateTime(2014, 10, 31).IsEndOfMonth());
+            Assert.True(new DateTime(2016, 2, 29).IsEndOfMonth());
+            Assert.True(new DateTime(2017, 2, 28).IsEndOfMonth());
+            Assert.True(new DateTime(2014, 11, 30).IsEndOfMonth());
+        }
+
+        [Fact]
+        public void FollowingMonths()
+        {
+            Assert.True(new DateTime(2014, 10, 31)
+                .FollowingMonths()
+                .Take(5)
+                .SequenceEqual(new[]
+                {
+                    new DateTime(2014, 11, 30),
+                    new DateTime(2014, 12, 31),
+                    new DateTime(2015, 1, 31),
+                    new DateTime(2015, 2, 28),
+                    new DateTime(2015, 3, 31)
+                }));
+
+            Assert.True(new DateTime(2014, 10, 23)
+                .FollowingMonths()
+                .Take(5)
+                .SequenceEqual(new[]
+                {
+                    new DateTime(2014, 11, 23),
+                    new DateTime(2014, 12, 23),
+                    new DateTime(2015, 1, 23),
+                    new DateTime(2015, 2, 23),
+                    new DateTime(2015, 3, 23)
+                }));
+
+            Assert.True(new DateTime(2014, 10, 31)
+                .FollowingMonths(2)
+                .Take(5)
+                .SequenceEqual(new[]
+                {                    
+                    new DateTime(2014, 12, 31),                    
+                    new DateTime(2015, 2, 28),
+                    new DateTime(2015, 4, 30),
+                    new DateTime(2015, 6, 30),
+                    new DateTime(2015, 8, 31)
+                }));
+
+            Assert.True(new DateTime(2014, 10, 23)
+                .FollowingMonths(2)
+                .Take(5)
+                .SequenceEqual(new[]
+                {                    
+                    new DateTime(2014, 12, 23),
+                    new DateTime(2015, 2, 23),
+                    new DateTime(2015, 4, 23),
+                    new DateTime(2015, 6, 23),
+                    new DateTime(2015, 8, 23)
+                }));
+        }
+
+        [Fact]
+        public void DateTimeFollowingEndOfMonths()
+        {
+            Assert.True(new DateTime(2014, 10, 31)
+                .FollowingEndOfMonths()
+                .Take(5)
+                .SequenceEqual(new[]
+                {
+                    new DateTime(2014, 11, 30),
+                    new DateTime(2014, 12, 31),
+                    new DateTime(2015, 1, 31),
+                    new DateTime(2015, 2, 28),
+                    new DateTime(2015, 3, 31)
+                }));
+
+            Assert.True(new DateTime(2014, 10, 23)
+                .FollowingEndOfMonths()
+                .Take(5)
+                .SequenceEqual(new[]
+                {
+                    new DateTime(2014, 10, 31),
+                    new DateTime(2014, 11, 30),
+                    new DateTime(2014, 12, 31),
+                    new DateTime(2015, 1, 31),
+                    new DateTime(2015, 2, 28)                    
+                }));
+
+            Assert.True(new DateTime(2014, 10, 31)
+                .FollowingEndOfMonths(2)
+                .Take(5)
+                .SequenceEqual(new[]
+                {                    
+                    new DateTime(2014, 12, 31),                    
+                    new DateTime(2015, 2, 28),
+                    new DateTime(2015, 4, 30),
+                    new DateTime(2015, 6, 30),
+                    new DateTime(2015, 8, 31)
+                }));
+
+            Assert.True(new DateTime(2014, 10, 23)
+                .FollowingEndOfMonths(2)
+                .Take(5)
+                .SequenceEqual(new[]
+                {                    
+                    new DateTime(2014, 12, 31),                    
+                    new DateTime(2015, 2, 28),
+                    new DateTime(2015, 4, 30),
+                    new DateTime(2015, 6, 30),
+                    new DateTime(2015, 8, 31)
+                }));
+        }
+    }
+}
---
+++ test/Next.Tests/EnumerableExtFixture.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Next.Tests
+{
+    public sealed class EnumerableExtFixture
+    {
+        [Fact]
+        public void EnumerableFollow()
+        {
+            var sequnce = new[] { 1, 2, 3 }
+                .Follow(4);
+
+            Assert.Equal(4, sequnce.Last());
+        }
+
+        [Fact]
+        public void ToEnumerable()
+        {
+            Assert.Equal(1, 1.ToEnumerable().First());
+            Assert.Equal(1, 1.ToEnumerable().Last());
+        }
+    }
+}

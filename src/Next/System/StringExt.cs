9d1a53306262bf8129267dc989bf3ffe6cced822
unknown <n.pachuashvili@NIKOLOZ-PC1.altasoft.local>
2015-06-17 10:34:30 +0400

    Changed file location

---
+++ src/Next/System/StringExt.cs
@@ -0,0 +1,31 @@
+﻿ // ReSharper disable once CheckNamespace
+namespace System
+{
+    /// <summary>
+    /// String extension methods
+    /// </summary>
+    public static class StringExt
+    {
+        /// <summary>        
+        /// Splits source string by specified separator and removes empty entries.
+        /// </summary>
+        /// <param name="this">Source string</param>
+        /// <param name="separator">Separator</param>
+        /// <returns>Array of strings</returns>
+        public static string[] SplitAndRemoveEmptyEntries(this string @this, params char[] separator)
+        {
+            return @this.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        /// <summary>
+        /// Splits source string by specified separator and removes empty entries.
+        /// </summary>
+        /// <param name="this">Source string</param>
+        /// <param name="separator">Separator</param>
+        /// <returns>Array of strings</returns>
+        public static string[] SplitAndRemoveEmptyEntries(this string @this, params string[] separator)
+        {
+            return @this.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}

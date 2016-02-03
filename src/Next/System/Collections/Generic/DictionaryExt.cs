d2002a298af2f43a96cfc0379f320413a1a6c9e9
Nikoloz Pachuashvili <nikoloz@hotmail.com>
2016-02-03 11:53:36 +0400

    Added new extension method

---
+++ src/Next/System/Collections/Generic/DictionaryExt.cs
@@ -0,0 +1,16 @@
+﻿ // ReSharper disable once CheckNamespace
+namespace System.Collections.Generic
+{
+    /// <summary>
+    /// IDictionary extension methods
+    /// </summary>
+    public static class DictionaryExt
+    {        
+        public static Tuple<bool, TValue> TryGetValue<TKey, TValue>(this IDictionary<TKey, TValue> self, TKey key)
+        {
+            TValue value;
+
+            return Tuple.Create(self.TryGetValue(key, out value), value);
+        }
+    }
+}

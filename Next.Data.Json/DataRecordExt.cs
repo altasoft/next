d19a67d5d94f039ce3d40fefa6c6758d70ede779
Nikoloz Pachuashvili <nikoloz@hotmail.com>
2018-10-17 17:02:24 +0400

    Added: Next.Data.Json

---
+++ Next.Data.Json/DataRecordExt.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+// ReSharper disable once CheckNamespace
+namespace System.Data
+{
+    public static class DataRecordExt
+    {
+        #region Json
+
+        public static TResult ReadJsonAs<TResult>(this IDataRecord self, string name, TypeNameHandling typeNameHandling = TypeNameHandling.Auto)
+            where TResult : class => ReadJsonAs<TResult>(self, name, new JsonSerializerSettings { TypeNameHandling = typeNameHandling });
+
+        public static TResult ReadJsonAs<TResult>(this IDataRecord self, string name, JsonSerializerSettings serializerSettings)
+            where TResult : class => self.GetString(name).IfNotNull(v => JsonConvert.DeserializeObject<TResult>(v, serializerSettings));
+
+        #endregion
+    }
+}
--- src/Next.Data/System/Data/DataRecordExt.cs
+++ src/Next.Data/System/Data/DataRecordExt.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Xml;
-using Newtonsoft.Json;
 
 // ReSharper disable once CheckNamespace
 namespace System.Data
@@ -140,17 +139,7 @@ namespace System.Data
         }
 
         #endregion
-
-        #region Json
-
-        public static TResult ReadJsonAs<TResult>(this IDataRecord self, string name, TypeNameHandling typeNameHandling = TypeNameHandling.Auto)
-            where TResult : class => ReadJsonAs<TResult>(self, name, new JsonSerializerSettings { TypeNameHandling = typeNameHandling });
-
-        public static TResult ReadJsonAs<TResult>(this IDataRecord self, string name, JsonSerializerSettings serializerSettings)
-            where TResult : class => self.GetString(name).IfNotNull(v => JsonConvert.DeserializeObject<TResult>(v, serializerSettings));
-
-        #endregion
-
+        
         #region Xml
 
         public static TResult ReadXmlAs<TResult>(this IDataRecord self, string name)
